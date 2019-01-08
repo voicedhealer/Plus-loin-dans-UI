7b19135d352df95864717c36d53f4a4164944331
voicedhealer <vivien.bernardot@gmail.com>
2019-01-08 17:13:04 +0100

    UILabel

--- Plus loin dans UI/ProgrammatiqueController.swift
+++ Plus loin dans UI/ProgrammatiqueController.swift
@@ -11,30 +11,29 @@ import UIKit
 class ProgrammatiqueController: UIViewController {
     
     var monPremierUIView: UIView?
+    var monPremierLabel : UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         monPremierUIView = UIView(frame: CGRect(x: 30, y: 60, width: 200, height: 50))
-        monPremierUIView?.backgroundColor = UIColor(red: 30 / 255, green: 123 / 255, blue: 90 / 255, alpha: 1)
+        monPremierUIView?.backgroundColor = UIColor(red: 22 / 255, green: 200 / 255, blue: 76 / 255, alpha: 1)
         monPremierUIView?.layer.cornerRadius = 25
         guard monPremierUIView != nil else { return }
         view.addSubview(monPremierUIView!)
-        print(monPremierUIView?.frame)
-        print(monPremierUIView?.bounds)
+       
+        monPremierLabel = UILabel(frame: CGRect(x: 0, y: monPremierUIView!.frame.maxY + 20, width: view.frame.width, height: 50))
+        monPremierLabel?.text = "Coucou je suis un texte"
+        monPremierLabel?.numberOfLines = 1
+        monPremierLabel?.textColor = UIColor.white
+        monPremierLabel?.font = UIFont(name: "Chalkduster", size: 20)
+        monPremierLabel?.textAlignment = .center
+        view.addSubview(monPremierLabel!)
         
-        let secondeVue = UIView(frame: monPremierUIView!.frame)
-        secondeVue.center.y += 100
-        secondeVue.backgroundColor = UIColor.red
-        view.addSubview(secondeVue)
         
-        let troisiemeVue = UIView(frame: monPremierUIView!.bounds)
-        troisiemeVue.center.y += 300
-        troisiemeVue.backgroundColor = UIColor.brown
-        view.addSubview(troisiemeVue)
         
-    }
+    
     
 
    
-
+    }
 }
